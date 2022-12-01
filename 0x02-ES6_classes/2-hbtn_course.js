e531ee412aa046b8cbbe425a40235706993e1358
JSM788 <salinasjuan788@gmail.com>
2022-12-01 16:51:01 -0500

    Adding 2-hbtn_course.js file

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,38 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof instance === 'string') throw TypeError('Name must be a string');
+    if (typeof instance === 'number') throw TypeError('Length must be a number');
+    if (Array.isArray === true) throw TypeError('Students must be a array');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof instance === 'string') throw new TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof instance === 'number') throw new TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  set students(students) {
+    if (Array.isArray === true) throw new TypeError('Students must be a array');
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
