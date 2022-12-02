c4b09fbcfb70a51ff9bcd6a8941f1b93940af6ac
JSM788 <salinasjuan788@gmail.com>
2022-12-01 22:45:56 -0500

    Adding 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,11 @@
+export default class Building {
+  constructor(sqft) {
+    // [class Building] !== [class TestBuilding extends Building]
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
