edf6208a4e220b2e4b468771c6b22e5e530d533f
JSM788 <salinasjuan788@gmail.com>
2022-12-06 00:54:52 -0500

    Fixing condition

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(len, position, value) {
-  if (!value) throw new Error("Position outside range");
+  if (position >= len) throw new Error("Position outside range");
   const arraybuf = new ArrayBuffer(len);
   const dataView = new DataView(arraybuf);
   dataView.setUint8(position, value);
