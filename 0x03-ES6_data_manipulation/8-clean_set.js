a994c690b651e092ae096582d7de78addeb3bdc7
JSM788 <salinasjuan788@gmail.com>
2022-12-06 14:17:27 -0500

    Adding condition undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
+  if (typeof startString === 'undefined') return '';
   const array = [...set];
   const newArray = array.filter((item) => item !== undefined && item.startsWith('bon'));
   // newArray = [ 'bonjovi', 'bonaparte', 'bonappetit' ]
   const result = newArray.map((item) => item.split('bon').pop());
+  // [ [ '', 'jovi' ], [ '', 'aparte' ], [ '', 'appetit' ] ].pop()
   // result = [ 'jovi', 'aparte', 'appetit' ]
   return result.join('-');
 }
