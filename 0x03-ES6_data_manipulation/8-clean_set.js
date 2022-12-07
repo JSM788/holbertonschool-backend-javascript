fadc02418720ea8e5f5110da87dd18eb9d16117e
JSM788 <salinasjuan788@gmail.com>
2022-12-06 23:30:19 -0500

    Adding file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString && typeof startString === 'string') {
-    const array = [...set];
-    const newArray = array.filter((item) => typeof item === 'string' && item !== undefined && item && item.startsWith(startString));
-    const result = newArray.map((item) => item.split(startString).pop());
-    return result.join(' ');
-  }
-  return '';
+  if (!startString || typeof startString === 'undefined' || startString.length <= 0 || startString === ' ') return '';
+  const array = [...set];
+  const newArray = array.filter((item) => typeof item === 'string' && item !== undefined && item !== ' ' && item && item.startsWith(startString));
+  const result = newArray.map((item) => item.split(startString)).pop());
+  return result.join('-');
 }
