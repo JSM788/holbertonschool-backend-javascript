8c9bae167c8d337847b8bac81a09e76bbee7dfa0
JSM788 <salinasjuan788@gmail.com>
2022-12-06 23:32:31 -0500

    Up file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,6 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString === 'undefined' || startString.length <= 0 || startString === ' ') return '';
   const array = [...set];
   const newArray = array.filter((item) => typeof item === 'string' && item !== undefined && item !== ' ' && item && item.startsWith(startString));
-  const result = newArray.map((item) => item.split(startString)).pop());
+  const result = newArray.map((item) => item.split(startString).pop());
   return result.join('-');
 }
