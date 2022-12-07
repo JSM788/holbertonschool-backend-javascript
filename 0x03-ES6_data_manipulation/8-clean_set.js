1db305864c3adb4b8e0cf209860537fc143717de
JSM788 <salinasjuan788@gmail.com>
2022-12-06 22:55:36 -0500

    Fixing errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
-  const array = [...set];
-  const newArray = array.filter((item) => item !== undefined && item.startsWith(startString));
+  if (!startString && startString !== undefined) return '';
+  const arr = [...set];
+  const newArr = arr.filter((item) => item && item !== undefined && item.startsWith(startString));
   // newArray = [ 'bonjovi', 'bonaparte', 'bonappetit' ]
-  const result = newArray.map((item) => item.slice(startString.length));
+  const result = newArr.map((item) => item.slice(startString.length));
   // result = [ 'jovi', 'aparte', 'appetit' ]
   return result.join('-');
 }
