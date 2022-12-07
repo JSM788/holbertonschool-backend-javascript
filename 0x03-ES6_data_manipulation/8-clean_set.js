18e919281e30686628cf97e492c31de1496ed6fa
JSM788 <salinasjuan788@gmail.com>
2022-12-06 21:44:41 -0500

    Adding more conditions

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString && typeof startString === 'string') {
-    const array = [...set];
-    const newArray = array.filter((item) => typeof item === 'string' && item !== undefined && item && item.startsWith('bon'));
-    // newArray = [ 'bonjovi', 'bonaparte', 'bonappetit' ]
-    const result = newArray.map((item) => item.split('bon').pop());
-    // [ [ '', 'jovi' ], [ '', 'aparte' ], [ '', 'appetit' ] ].pop()
-    // result = [ 'jovi', 'aparte', 'appetit' ]
-    return result.join('-');
-  }
-  return '';
+  if (!startString || typeof startString === 'undefined' || startString.length <= 0) return '';
+  const array = [...set];
+  const newArray = array.filter((item) => typeof item === 'string' && item !== undefined && item && item.startsWith('bon'));
+  // newArray = [ 'bonjovi', 'bonaparte', 'bonappetit' ]
+  const result = newArray.map((item) => item.split('bon').pop());
+  // result = [ 'jovi', 'aparte', 'appetit' ]
+  return result.join('-');
 }
