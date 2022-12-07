c71bdf4e9a540d1fb85a115c0d091574dce8bace
JSM788 <salinasjuan788@gmail.com>
2022-12-06 22:08:16 -0500

    Adding slice function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString === 'undefined' || startString.length <= 0 || startString === ' ') return '';
+  if (!startString || typeof startString === 'undefined' || startString.length <= 0) return '';
   const array = [...set];
-  const newArray = []
-  for (const undf of array)
-    if (undf !== undefined && undf.startsWith('bon'))
-      newArray.push(undf)
-  const result = newArray.map((item) => item.split('bon').pop());
+  const newArray = [];
+  for (const undf of array) {
+    if (undf !== undefined && undf.startsWith('bon')) {
+      newArray.push(undf);
+    }
+  }
+  const result = newArray.map((item) => item.slice(startString.length));
   return result.join('-');
 }
