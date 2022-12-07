ae721d621418acba66b6814a65b5cc59197bf811
JSM788 <salinasjuan788@gmail.com>
2022-12-06 22:39:09 -0500

    Fixing filter

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString === 'undefined' || startString === undefined) return '';
   const array = [...set];
-  const newArray = array.filter((item) => typeof item !== 'undefined' && item.startsWith(startString));
+  const newArray = array.filter((item) => item.startsWith(startString));
   // newArray = [ 'bonjovi', 'bonaparte', 'bonappetit' ]
   const result = newArray.map((item) => item.slice(startString.length));
   // result = [ 'jovi', 'aparte', 'appetit' ]
